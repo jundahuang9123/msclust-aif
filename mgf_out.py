0a1330ef81044ce06715faea54a2f4cf1e40521b
Junda Huang <junda.huang@wur.nl>
2021-08-19 14:55:24 +0200

    mgf csv reader

--- mgf_out.py
+++ mgf_out.py
@@ -11,6 +11,26 @@ def mgf_test(mgf):
     file = load_from_mgf("msclust-aif/out.mgf")
     print(file)
 
+def aifcluster_read(file):
+    """
+    Read aifcluster information from csv file write into dictionary
+    Param:
+        file(string): file name of the csv file containing aifcluster info
+    output:
+        aif_dict(dict): dictionary containing aifcluster information
+                        {'Precursor ion': {'info' : [infos]}; {'fragment':}}
+    """
+    aif_dict = {}
+    headers_list = []
+    frag_list = []
+    with open (file, 'r') as aif_file:
+        for i, line in enumerate(aif_file):
+            items = line.strip('\n').split(',')
+            if i == 0:
+                headers_list = items[0:]
+                
+    return aif_dict
+
 def mgf_write(dict):
     mgf.write()
 
